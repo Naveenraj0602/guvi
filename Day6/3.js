a090cd3ad6ff44f696a4deba3a9484342f29b91b
naveen <naveenrajvimal@gmail.com>
2023-11-26 09:45:38 +0530

    Day6 task completed

---
+++ Day6/3.js
@@ -0,0 +1,10 @@
+ // 3.Write a “person” class to hold all the details.
+ class person {
+    constructor(name, age, gender, DOB) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+      this.DOB = DOB;
+    }
+  }
+ 

b24bd10d30c04df187b1177cbacc55a5b5fe505f
naveen <naveenrajvimal@gmail.com>
2023-12-27 22:27:23 +0530

    Day22task completed

---
+++ Day22/react-cards/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import PriceCard from './components/PriceCards'
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+    <PriceCard />
+    </>
+  )
+}
+
+export default App

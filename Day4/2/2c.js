03fa0c2498cf6911abaaf0641f2f61e9c3fbff8c
naveen <naveenrajvimal@gmail.com>
2023-12-27 22:48:09 +0530

    .

--- Day4/2/2c.js
+++
@@ -1,8 +0,0 @@
-const sumOfNumbers = (arr) => {
-    return arr.reduce((sum, num) => sum + num, 0);
-  };
-  
-  const numbers = [1, 2, 3, 4, 5];
-  const resultSum = sumOfNumbers(numbers);
-  console.log(resultSum);
-  

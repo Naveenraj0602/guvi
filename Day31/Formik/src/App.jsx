8bab85f37fb791a0f40ad1e639055c53fc5fe7e8
naveen <naveenrajvimal@gmail.com>
2024-02-05 09:12:53 +0530

    Day31

--- Day31/Formik/src/App.jsx
+++ Day31/Formik/src/App.jsx
@@ -1,26 +1,43 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Sidebar from './Components/Sidebar'
-import Navbar from './Components/Navbar'
-import Products from './Components/products';
-
-import './App.css'
+import React from 'react';
+import { LibraryProvider } from './context/librarycontext';
+import BookForm from './pages/bookform';
+import BookList from './pages/booklist';
+import AuthorForm from './pages/authorform';
+import AuthorList from './pages/authorlist';
+import './App.css'; 
 
 function App() {
-
   return (
-    <>
-   <div className='d-flex' style={{height:'100%'}}>
-   <Sidebar/>
-      <div className='' style={{width:'100%'}}>
-      <Navbar/>
-      <Products />
+    <LibraryProvider>
+      <div className="app-container">
+        <header>
+          <h1>Library Admin Dashboard</h1>
+        </header>
+        <main>
+          <section className="forms-section">
+            <div className="form-container">
+              <h2>Add Book</h2>
+              <BookForm />
+            </div>
+            <div className="form-container">
+              <h2>Add Author</h2>
+              <AuthorForm />
+            </div>
+          </section>
+          <section className="lists-section">
+            <div className="list-container">
+              <h2>Book List</h2>
+              <BookList />
+            </div>
+            <div className="list-container">
+              <h2>Author List</h2>
+              <AuthorList />
+            </div>
+          </section>
+        </main>
       </div>
-   </div>
-    
-   
-    </>
-  )
+    </LibraryProvider>
+  );
 }
 
-export default App
+export default App;
